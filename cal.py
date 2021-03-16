f2cfeca7b82b5d6d478729d1f127b827d00fcf00
Surakit2543 <76984652+Surakit2543@users.noreply.github.com>
2021-03-16 09:45:56 +0700

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,20 @@
+def add (x,y):
+    """Add Function"""
+    return x + y
+    
+
+def subtract (x,y):
+    """Subtract Fuction"""
+    return x - y
+
+
+def multiply (x,y):
+    """multiply Fuction"""
+    return x * y
+
+
+def divide (x,y):
+    """divide Fuction"""
+    if y == 0
+        raise ValueError('can not divide zero')
+    return x / y
